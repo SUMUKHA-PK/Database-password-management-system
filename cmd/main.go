2037fac1f0016b3c74a68a74150d95fcc72f843c
SUMUKHA-PK <sumukhapk46@gmail.com>
2019-06-15 10:47:56 +0530

    Migrated to own basic server repo

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"context"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
+	"github.com/SUMUKHA-PK/Basic-Golang-Server/server"
 	"github.com/SUMUKHA-PK/Database-password-management-system/routing"
 
 	"github.com/gorilla/mux"
-	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func main() {
@@ -21,45 +13,5 @@ func main() {
 
 	*r = routing.SetupRouting(*r)
 
-	server := &http.Server{
-		Handler: r,
-		Addr:    ":51515",
-	}
-
-	LogFileLocation := os.Getenv("LogFileLocation")
-	if LogFileLocation != "" {
-		log.SetOutput(&lumberjack.Logger{
-			Filename:   LogFileLocation,
-			MaxSize:    500, // megabytes
-			MaxBackups: 3,
-			MaxAge:     28,   //days
-			Compress:   true, // disabled by default
-		})
-	}
-
-	go func() {
-		log.Println("Starting Server on port 51515")
-		if err := server.ListenAndServe(); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	waitForShutdown(server)
-}
-
-// waitForShutdown shuts down the server on getting a ^C signal
-func waitForShutdown(server *http.Server) {
-	interruptChan := make(chan os.Signal, 1)
-	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-
-	// Block until we receive our signal.
-	<-interruptChan
-
-	// Create a deadline to wait for.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-	server.Shutdown(ctx)
-
-	log.Println("Shutting down")
-	os.Exit(0)
+	server.Server(r, "51515")
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/SUMUKHA-PK/Database-password-management-system
 go 1.12
 
 require (
+	github.com/SUMUKHA-PK/Basic-Golang-Server v0.0.0-20190615051531-13ca9882f184
 	github.com/SUMUKHA-PK/Mail-Server v0.0.0-20190609055344-90c73a193cb2
 	github.com/gorilla/mux v1.7.2
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
